293c765dd990b48ad3c35d992fe641252626b44d
= <s791623@outlook.com>
2025-10-02 22:41:52 -0600

    learning 02

---
+++ sandbox/jungleGym/Costume.cs
@@ -0,0 +1,16 @@
+class Costume
+{
+    // attributes
+    public string outfit = "";
+    public string footwear = "";
+    public string tools = "";
+    public string size = "";
+
+    // behaviors
+    public void Display()
+    {
+        Console.WriteLine($"outfit: {outfit}");
+        Console.WriteLine($"footwear: {footwear}");
+        Console.WriteLine($"tool: {tools}");
+    }
+}
--- sandbox/jungleGym/Program.cs
+++ sandbox/jungleGym/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        Costume myCostume = new();
+        mycostume.outfit = "tartan and kilt";
+        myCostume.Display();
     }
 }
